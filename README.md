608f2e28ff95372dbb301b63d46e783d39e2932b
Chris Poche <capoche1@gmail.com>
2019-12-04 10:01:14 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+RSMFValidatorSampleCode
+=======
+
+## Introduction
+
+The RSMFValidatorSampleCode project illustrates how to leverage the RSMF Validator library provided by Relativity.  The RSMF JSON schema is included in the RSMFManifestSchema directory.
+
+## Desktop Client EXE
+
+The Desktop Client, RSMFValidationDesktop, was created on top of the Console Application RSMFValidationSampleCode. It has been built and is ready to be run from the GUI. It can be found under .\rsmf-validator-samples-master\RSMFValidationDesktop\bin\Debug\netcoreapp3.0\
+
+Note: My contribution is the WPF app, not the Console app.
+
+1. Run the RSMFValidationDesktop Application
+2. Press 'Select RSMF Sample to Validate' button, this will open a file dialog
+3. Select any file within the target folder you want to test, if there are multiple json files, just choose one at random, it'll run against them all 
+4. Validation will happen in the text box below the button and can be found in .\rsmf-validator-samples-master\RSMFValidationDesktop\bin\Debug\netcoreapp3.0\rsmf.validation.log for later viewing. 
+
+## Build
+
+To build the sample code, open the RSMFValidatorSampleCode.sln in Visual Studio 2015.  Restore the Nuget packages which will download and install the RSMF Validator library.  Build in release or debug mode.
+
+## RSMF sample files
+
+A short walkthrough with invalid RSMF sample files is included in the Documentation directory.  See RSMFValidatorSampleCode.pdf.
---
+++ RSMFValidationDesktop/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.IO;
+using Microsoft.Win32;
+
+using RSMFValidationSampleCode;
+
+
+namespace RSMFValidationDesktop.Dialogs
+{
+    public partial class OpenFileDialogSample : Window
+    {
+        public OpenFileDialogSample()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string[] directoryPath = new String[1] { openFileDialog.FileName.Replace($"\\{openFileDialog.SafeFileName}", "") };
+                SampleProgram.Main(directoryPath);
+                string logLocation = System.Reflection.Assembly.GetEntryAssembly().Location;
+                logLocation = logLocation.Replace("RSMFValidationDesktop.dll", newValue: "rsmf.validation.log.txt");
+                //txtEditor.Text = logLocation;
+                //txtEditor.Text = directoryPath;
+                txtEditor.Text = File.ReadAllText(logLocation);
+            }
+        }
+    }
+}
